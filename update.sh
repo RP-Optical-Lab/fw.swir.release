e54d3412b993f8b4a9b7b52e53ad7ee85b78199c
Max Parker <max@rp-optical-lab.co.il>
2024-02-22 00:02:18 -0800

    FF

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@
 usage () {
 	echo "# usage:   ./update.sh fw-name"
 	echo "# example: ./update.sh rp-swir-sdi-isp-v001a"
+	echo
 	exit 1
 }
 
@@ -11,12 +12,13 @@ say_and_do () {
 	eval $@
 	if [ $? -ne 0 ]; then
 		echo "# - error: Command failed!"
+		echo
 		exit 1
 	fi
 }
 
 echo "##############################################"
-echo ""
+echo
 
 FW="$1"
 if [ -z "$FW" ]; then
@@ -26,21 +28,21 @@ else
 	echo "# - FW found, setting $FW"
 fi
 
-# echo "# fw - download"
+# echo "# download"
 # say_and_do git clone https://github.com/RP-Optical-Lab/fw.swir.release.git
 # say_and_do cd fw.swir.release
 say_and_do tar xvf $FW.tar.gz
-say_and_do sudo cp -r $FW /lib/firmware/xilinx
-say_and_do cd ..
+say_and_do sudo mv $FW /lib/firmware/xilinx
+# say_and_do cd ..
 echo
 
-echo "# fw - update firmware"
-say_and_do 'sudo echo "$FW" > /etc/dfx-mgrd/$FW'
+echo "# update"
+say_and_do 'echo '$FW' | sudo tee /etc/dfx-mgrd/'$FW''
 say_and_do sudo ln -sf /etc/dfx-mgrd/$FW /etc/dfx-mgrd/default_firmware
 say_and_do ls -l /etc/dfx-mgrd
 say_and_do more /etc/dfx-mgrd/$FW
 
-say_and_do rm -rf $FW
-
+echo
 echo "# - done"
+echo "# - restart the system"
 echo
